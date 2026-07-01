b2ee9b23d98187a1d998ecd38099f3a306afc814
Simon Luu <simonluu@Simons-MacBook-Pro-13.local>
2026-07-01 16:21:44 -0400

    fixed type in php file

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo '<p>Options: ' . htmlspecialchars(implode(', ', $options)) . '</p>';
 echo '<h2>All Form Data</h2>';
 echo '<pre>';
 foreach ( $_POST as $key => $value ) {
-    echo '<p>Options: ' . htmlspecialchar( implode( ', ', $$value ) ) . '</p>';
+    echo '<p>Options: ' . htmlspecialchars( implode( ', ', $$value ) ) . '</p>';
 }
 echo '</pre>';
 ?>
